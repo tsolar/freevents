a64a171a2f80248872b8258f63485f54d056e12b
Tomás Solar Castro <tsolar@gmail.com>
2018-12-01 17:29:09 -0300

    fix view test with apostrophe

--- spec/views/entity/organizations/show.html.slim_spec.rb
+++ spec/views/entity/organizations/show.html.slim_spec.rb
@@ -4,11 +4,13 @@ require "rails_helper"
 
 RSpec.describe "entity/organizations/show", type: :view do
   before do
-    @entity_organization = assign(:entity_organization, create(:entity_organization))
+    @entity_organization = assign(
+      :entity_organization, create(:entity_organization, name: "o'clock")
+    )
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/#{@entity_organization.name}/)
+    expect(rendered).to match(/#{CGI.escapeHTML(@entity_organization.name)}/)
   end
 end
